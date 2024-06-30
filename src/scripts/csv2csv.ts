37706a65fe149b3c312fdfb116222e328f0ab994
Yarix <yaroslav@chikal.uk>
2024-06-30 12:46:14 +0500

    Changes

--- src/scripts/csv2csv.ts
+++ src/scripts/csv2csv.ts
@@ -24,22 +24,33 @@ const readCSV = (filePath: string): Promise<any[]> => {
 };
 
 const findAddressesInCSVFiles = async () => {
-    console.log("\n======== Processing CSV Files ========\n");
+    console.log("\n======== Processing CSV Files ========");
 
     // Чтение файла coinbase.csv
     const addressFilePath = path.resolve(__dirname, coinbaseCsv);
     const coinbaseAddresses = await readCSV(addressFilePath);
 
     // Создание словаря для адресов coinbase.csv
+    console.log("Coinbase addresses:\n");
     const addressMap: Record<string, string> = {};
     coinbaseAddresses.forEach(row => {
         addressMap[row.Address] = row.Name;
+        console.log(`${row.Name} ${row.Address}`);
     });
+    console.log("\n==========================================================");
 
     // Получение списка всех файлов в папке exports
     const directoryPath = path.resolve(__dirname, csvFolder);
     const files = fs.readdirSync(directoryPath).filter(file => file.endsWith('.csv'));
 
+    console.log("Exports list:\n");
+    for (let i = 0; i < files.length; i++) {
+        const filePath = path.resolve(directoryPath, files[i]);
+        console.log(filePath);
+    }
+    console.log("\n==========================================================");
+
+    console.log("Findings list:\n");
     for (let i = 0; i < files.length; i++) {
         const filePath = path.resolve(directoryPath, files[i]);
         const transactions = await readCSV(filePath);
@@ -49,7 +60,7 @@ const findAddressesInCSVFiles = async () => {
                 const toName = addressMap[tx.To];
 
                 if (fromName || toName) {
-                    console.log(`${index + 1}. From: ${fromName || tx.From}, To: ${toName || tx.To}, File: ${files[i]}\n`);
+                    console.log(`Row: ${index + 1}. From: ${fromName || tx.From}, To: ${toName || tx.To}, File: ${files[i]}`);
                 } else {
                     // console.log(`Address ${tx.From} nor ${tx.To} not found in coinbase.csv`);
                 }
@@ -59,7 +70,7 @@ const findAddressesInCSVFiles = async () => {
         }
     }
 
-    console.log("======== Processing Complete ========\n");
+    console.log("\n======== Processing Complete ========\n");
 };
 
 // Запуск функции поиска адресов в CSV файлах
